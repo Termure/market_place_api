5a3a63aaeb890fbe0a862eb799a7f6d930eb8eea
Ioan Termure <ioan.termure@unifiedpost.com>
2022-05-09 14:24:40 +0300

    Refactor GET user specs

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
   describe "CRUD" do
-    let!(:user) { create :user}
+    context 'GET user' do
+      let!(:user) { create :user}
 
-    it 'GET user' do
-      get "/api/v1/users/#{user.id}", as: :json # or get api_v1_user_url(user)
-      expect(response).to have_http_status(:success)
-      json_response = JSON.parse(response.body)
-      expect(json_response['email']).to eq(user.email)
+      before do
+        get "/api/v1/users/#{user.id}", as: :json # or get api_v1_user_url(user)
+      end
+
+      it 'responds with status success (200)' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns the correct user' do
+        json_response = JSON.parse(response.body)
+        expect(json_response['email']).to eq(user.email)
+      end
     end
   end
 end
