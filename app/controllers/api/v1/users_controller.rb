837460990cc10e308162b5795ae0781dab69684a
Ioan Termure <ioan.termure24@gmail.com>
2022-05-18 13:15:31 +0300

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::UsersController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActiveRecord::RecordInvalid,  with: :render_422
+  before_action :set_user, only: %i(show update)
 
-  # GET /users/1
+    # GET /users/1
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   # POST /users/1
@@ -13,7 +14,19 @@ class Api::V1::UsersController < ApplicationController
     render_201 if @user.save!
   end
 
+  def update
+    if @user.update(user_params)
+      render_200
+    else
+      render_422
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:email, :password)
@@ -29,5 +42,9 @@ class Api::V1::UsersController < ApplicationController
 
   def render_201
     render json: @user, status: :created
+  end
+
+  def render_200
+    render json: @user, status: :ok
   end
 end
