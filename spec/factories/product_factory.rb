37449186d0792a3048674abac73d08da5d46199c
Ioan Termure <ioan.termure24@gmail.com>
2022-05-27 14:18:05 +0300

    Adds some validations to products

--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :product do
     title { Faker::Book.title }
-    price { Faker::Number.decimal }
+    price { rand(0..2**32 - 1) }
     published { Faker::Boolean }
+    user
+
+    before :create do
+      create :user
+    end
   end
 end
