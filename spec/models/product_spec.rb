d592f7ecf1dfa1f33c43037c3946b058d6d1ecdd
Ioan Termure <ioan.termure@unifiedpost.com>
2022-05-27 14:18:05 +0300

    Adds some validations to products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+  describe 'validations' do
+    let!(:product) { create :product }
 
+    context 'validates product based on price' do
+      it 'product should be invalid when price is negative' do
+        product.price = -1
+        expect(product.valid?).to eql false
+      end
+
+      it 'product should be valid when price is positive' do
+        product.price = 10
+        expect(product.valid?).to eql true
+      end
+    end
+  end
 end
