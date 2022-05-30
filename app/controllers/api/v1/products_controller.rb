4194da945b511a491c85a73dfe437d85a2866d14
Ioan Termure <ioan.termure24@gmail.com>
2022-05-30 10:59:39 +0300

    Implements create product action

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   before_action :set_product, only: [:show]
+  before_action :check_login, only: [:create]
 
   def index
     render json: Product.all
@@ -10,6 +11,15 @@ class Api::V1::ProductsController < ApplicationController
     render json: @product
   end
 
+  def create
+    product = current_user.products.build(product_params)
+    if product.save
+      render json: product, status: :created
+    else
+      render json: { errors: product.errors }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_product
@@ -17,7 +27,7 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:title, :price, :published, :user_id)
+    params.require(:product).permit(:title, :price, :published)
   end
 
   def render_404
