795179559f168bd1eebdd854ba0f2646f37862b0
Ioan Termure <ioan.termure@unifiedpost.com>
2022-05-30 10:59:39 +0300

    Implements create product action

--- spec/controllers/api/v1/products_spec.rb
+++ spec/controllers/api/v1/products_spec.rb
@@ -4,6 +4,17 @@ RSpec.describe "Api::V1::Products", type: :request do
   describe "CRUD" do
     let!(:product) { create :product }
 
+    let!(:product_params) do
+      {
+        product: {
+          title:     product.title,
+          price:     product.price,
+          published: product.published,
+          user_id:   product.user_id
+        }
+      }
+    end
+
     context 'GET product' do
       it 'returns the product' do
         get api_v1_product_url(product), as: :json
@@ -21,6 +32,25 @@ RSpec.describe "Api::V1::Products", type: :request do
       it 'should show products' do
         get api_v1_products_url()
         expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'POST products' do
+      it 'creates the product and returns correct response' do
+        expect do
+          post api_v1_products_url(product_params),
+               headers: { Authorization: JsonWebToken.encode(user_id: product.user_id) }
+          expect(response).to have_http_status(:created)
+        end.to change(Product, :count).by(1)
+        json_response = JSON.parse(response.body)
+        expect(product.title).to eql(json_response['title'])
+      end
+
+      it 'not creates the product' do
+        expect do
+          post api_v1_products_url(product_params)
+          expect(response).to have_http_status(:forbidden)
+        end.to change(Product, :count).by(0)
       end
     end
   end
