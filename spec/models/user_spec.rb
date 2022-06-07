bbffb7d4c504d759f549e51b3c248d74c182b915
Ioan Termure <ioan.termure24@gmail.com>
2022-06-07 21:22:06 +0300

    Adds search scopes on the product model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,4 +27,31 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe 'filter products' do
+    let!(:product_one)   { create :product, title: 'Tv 1',  price: 300 }
+    let!(:product_two)   { create :product, title: 'BBC 2', price: 100 }
+    let!(:product_three) { create :product, title: 'Tv 3',  price: 200 }
+
+    it 'filters products by title' do
+      expect(Product.filter_by_title('tv').count).to eql 2
+    end
+
+    it 'filters products by title and sort them' do
+      expect(Product.filter_by_title('tv').sort_by(&:id)).to match([product_one, product_three])
+    end
+
+    it 'filters products with price equal or above to given price' do
+      expect(Product.above_or_equal_to_price(200).sort_by(&:price)).to match([product_three, product_one])
+    end
+
+    it 'filters products with price equal or lower to given price' do
+      expect(Product.bellow_or_equal_to_price(200).sort_by(&:price)).to match([product_two, product_three])
+    end
+
+    it 'sorts products by most recent' do
+      product_two.update(price: 33)
+      expect(Product.recent.to_a).to match([product_one, product_three, product_two])
+    end
+  end
 end
