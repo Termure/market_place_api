1d48bbed3f254656e37df4d0597ac054d5a19bdd
Ioan Termure <ioan.termure@unifiedpost.com>
2022-06-07 21:22:06 +0300

    Adds search scopes on the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ApplicationRecord
   belongs_to :user
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
   validates :title, :user_id, presence: true
+
+  scope :filter_by_title,          ->(keyword) { where('lower(title) LIKE ?', "%#{keyword.downcase}%") }
+  scope :above_or_equal_to_price,  ->(value) { where('price >= ?', value) }
+  scope :bellow_or_equal_to_price, ->(value) { where('price <= ?', value) }
+  scope :recent,                   -> { order(:updated_at) }
 end
