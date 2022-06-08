d59ec46bc13c03c1f05a5e50061e2b0873c887f5
Ioan Termure <ioan.termure24@gmail.com>
2022-06-08 21:20:40 +0300

    Adds search class method to filter products

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,14 @@ class Product < ApplicationRecord
   scope :above_or_equal_to_price,  ->(value) { where('price >= ?', value) }
   scope :bellow_or_equal_to_price, ->(value) { where('price <= ?', value) }
   scope :recent,                   -> { order(:updated_at) }
+
+  class << self
+    def search(params = {})
+      products = params[:prod_ids].present? ? Product.where(id: params[:prod_ids]) : Product.all
+      products = products.filter_by_title(params[:keyword]) if params[:keyword]
+      products = products.above_or_equal_to_price(params[:min_price].to_f) if params[:min_price]
+      products = products.bellow_or_equal_to_price(params[:max_price].to_f) if params[:max_price]
+      products
+    end
+  end
 end
